f1d8046c78d774481b0980a5b83e15f8538f1a89
Raunak Das <raunak.dvm@gmail.com>
2020-09-21 21:51:26 +0530

    Final Script

--- get-quote.py
+++ get-quote.py
@@ -8,7 +8,9 @@ def raunak():
 
   last = len(quotes)-1
   rnd = random.randint(0, last)
-  print(quotes[14])
-
+  quotes.replace("/n", " ")
+  print(quotes[rnd])
+  print(quotes[rnd-2])
+  
 if __name__== "__main__":
   raunak()
